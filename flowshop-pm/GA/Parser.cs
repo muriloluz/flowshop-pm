301ad0dded3f17b98ec951147813b681dd432eb2
muriloluz <murlop@gmail.com>
2022-08-21 21:12:26 -0300

    Ajustando

--- flowshop-pm/GA/Parser.cs
+++ flowshop-pm/GA/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,80 +11,99 @@ namespace flowshop_pm.GA
     {
         public InfoFlowShop Ler()
         {
-            var totalJobs = 5;
-            var totalMaquinas = 3;
+            var currentDir = Environment.CurrentDirectory + "\\data\\instancias_disp\\";
+
+            var prefixoArquivo = "RGd_20_5_2_25_1";
+
+            var files = Directory.GetFiles(currentDir, prefixoArquivo + ".txt", SearchOption.AllDirectories);
+
+
+            foreach (var file in files)
+            {
+                var linhasArquivo = File.ReadAllLines(file);
+                var linhaCorrente = 0;
+
+                var totalJobs = int.Parse(linhasArquivo[linhaCorrente].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries)[0].Trim());
+                var totalMaquinas = int.Parse(linhasArquivo[linhaCorrente].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries)[1].Trim());
+
+                var tabelProcessamento = new int[totalJobs][];
+
+            
+                for(int j = 0; j < totalJobs; j++)
+                {
+                    linhaCorrente++;
+                    tabelProcessamento[j] = new int[totalMaquinas];
+                    var valores = linhasArquivo[linhaCorrente].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    var posicaoEquivalente = 1;
+                    for (int m = 0; m < totalMaquinas; m++)
+                    {
+                        /// O arquivos contém o proprio índice intercalado
+                        tabelProcessamento[j][m] = int.Parse(valores[posicaoEquivalente]);
+                        posicaoEquivalente += 2;
+                    }
+                }
+
+                linhaCorrente++;
+
+                //// Agora deve iniciar a leitura do TPM
+                if(linhasArquivo[linhaCorrente].Trim() != "TPM")
+                {
+                    throw new Exception("Algo inesperado no arquivo");
+                }
+
+                linhaCorrente++;
+
+                /// TODO: Valores do tempo máximo da manutenção
+                var tabelaPeriodoManutecao = new int[totalMaquinas];
+
+
+                linhaCorrente++;
+
+                //// Agora deve iniciar a leitura do TPM
+                if (linhasArquivo[linhaCorrente].Trim() != "PMT")
+                {
+                    throw new Exception("Algo inesperado no arquivo");
+                }
+
+                //// Tempo gasto das manuteções
+
+                linhaCorrente++;
+
+                //// Quantidade manutenções estimadas
+                var tabelaManutencoes = new int[totalJobs / 2][];
+
+                var sequenciaManutencao = 0;
+
+                while (linhaCorrente < linhasArquivo.Count())
+                {
+                    var valores = linhasArquivo[linhaCorrente].Trim().Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+
+                    var posicaoEquivalente = 1;
+
+                    tabelaManutencoes[sequenciaManutencao] = new int[totalMaquinas];
+
+                    for (int m = 0; m < totalMaquinas; m++)
+                    {
+                        /// O arquivos contém o proprio índice intercalado
+                        tabelaManutencoes[sequenciaManutencao][m] = int.Parse(valores[posicaoEquivalente]);
+                        posicaoEquivalente += 2;
+                    }
+
+                    sequenciaManutencao++;
+                    linhaCorrente++;
+                }
+            }
+
             var tempoParaManutecao = 120;
             var manutencoesMaximas = 2;
 
-            var tabelProcessamento = new int[totalMaquinas][];
+            //var infoJobs = new InfoJobs(tabelProcessamento);
+            //var infoManutencoes = new InfoManutencaoMaquina(tabelaManutecoes);
 
-            if (tabelProcessamento[0] == null)
-            {
-                tabelProcessamento[0] = new int[totalJobs];
-            }
+            //var infoFlowShop = new InfoFlowShop(infoJobs, infoManutencoes, totalJobs, totalMaquinas, tempoParaManutecao, manutencoesMaximas);
 
-            tabelProcessamento[0][0] = 3;
-            tabelProcessamento[0][1] = 45;
-            tabelProcessamento[0][2] = 84;
-            tabelProcessamento[0][3] = 24;
-            tabelProcessamento[0][4] = 39;
-
-
-            if (tabelProcessamento[1] == null)
-            {
-                tabelProcessamento[1] = new int[totalJobs];
-            }
-
-            tabelProcessamento[1][0] = 38;
-            tabelProcessamento[1][1] = 51;
-            tabelProcessamento[1][2] = 7;
-            tabelProcessamento[1][3] = 21;
-            tabelProcessamento[1][4] = 16;
-
-
-            if (tabelProcessamento[2] == null)
-            {
-                tabelProcessamento[2] = new int[totalJobs];
-            }
-
-            tabelProcessamento[2][0] = 52;
-            tabelProcessamento[2][1] = 83;
-            tabelProcessamento[2][2] = 19;
-            tabelProcessamento[2][3] = 6;
-            tabelProcessamento[2][4] = 28;
-
-            var tabelaManutecoes = new int[totalMaquinas][];
-
-            if (tabelaManutecoes[0] == null)
-            {
-                tabelaManutecoes[0] = new int[manutencoesMaximas];
-            }
-
-            tabelaManutecoes[0][0] = 31;
-            tabelaManutecoes[0][1] = 43;
-
-            if (tabelaManutecoes[1] == null)
-            {
-                tabelaManutecoes[1] = new int[manutencoesMaximas];
-            }
-
-            tabelaManutecoes[1][0] = 45;
-            tabelaManutecoes[1][1] = 42;
-
-            if (tabelaManutecoes[2] == null)
-            {
-                tabelaManutecoes[2] = new int[manutencoesMaximas];
-            }
-            tabelaManutecoes[2][0] = 8;
-            tabelaManutecoes[2][1] = 45;
-
-
-            var infoJobs = new InfoJobs(tabelProcessamento);
-            var infoManutencoes = new InfoManutencaoMaquina(tabelaManutecoes);
-
-            var infoFlowShop = new InfoFlowShop(infoJobs, infoManutencoes, totalJobs, totalMaquinas, tempoParaManutecao, manutencoesMaximas);
-
-            return infoFlowShop;
+            return null;
         }
     }
 }
